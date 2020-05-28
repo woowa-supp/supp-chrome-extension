139e474279ed78e190bf8109b3abc743a420e221
K.S.KIM <nullable@kakao.com>
2020-05-28 16:25:22 +0900

    fix: modal이 사라진 후 다시 버튼을 눌렀을 때 화면이 출력되지 않는 현상 수정

--- src/js/ProfileExtension.js
+++ src/js/ProfileExtension.js
@@ -1,95 +1,97 @@
+/* modules area */
 // import { findSurveyResultByUserName } from "./modules/MockProfileApi.js";
 // import { MUTATION_OBSERVE_CONFIG } from "../utils/constants.js";
 // import { profileTemplate } from "../utils/templates.js";
 
-/* modules area */
-
 const profileTemplate = profile => 
 `
-<div class="v-card v-sheet theme--light">
-    <div class="v-card__title headline">
-        <button type="button" class="absolute top-30 v-btn v-btn--flat v-btn--text theme--light v-size--default">
-            <span class="v-btn__content">
-                <i aria-hidden="true" class="v-icon notranslate font-size-14 v-icon--left mdi mdi-arrow-left theme--light"></i> 뒤로
-            </span>
-        </button>
-        <span class="text-center width-100 mt-4">${profile["login"]}</span> 
-    </div>
-    <div class="mx-auto my-3"></div>
-    <div class="v-card__text text-center">
-        <div class="row">
-            <div class="col col-12">
+<div class="v-dialog v-dialog--active" style="width: 500px;">
+    <div class="v-card v-sheet theme--light">
+        <div class="v-card__title headline">
+            <button type="button" id="back-button" class="absolute top-30 v-btn v-btn--flat v-btn--text theme--light v-size--default">
+                <span class="v-btn__content">
+                    <i aria-hidden="true" class="v-icon notranslate font-size-14 v-icon--left mdi mdi-arrow-left theme--light"></i> 뒤로
+                </span>
+            </button>
+            <span class="text-center width-100 mt-4">${profile["login"]}</span> 
+        </div>
+        <div class="mx-auto my-3"></div>
+        <div class="v-card__text text-center">
+            <div class="row">
+                <div class="col col-12">
 
-            <div class="v-card__title">개발자 타입</div>
-            <div class="v-card__text">
-                <div class="text-left">
-                    <div class="tui-editor-contents">
-                        <p>${profile["devType"]}</p>
+                <div class="v-card__title">개발자 타입</div>
+                <div class="v-card__text">
+                    <div class="text-left">
+                        <div class="tui-editor-contents">
+                            <p>${profile["devType"]}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="v-card__title">운영체제</div>
-            <div class="v-card__text">
-                <div class="text-left">
-                    <div class="tui-editor-contents">
-                        <p>${profile["osStyle"]}</p>
+                <div class="v-card__title">운영체제</div>
+                <div class="v-card__text">
+                    <div class="text-left">
+                        <div class="tui-editor-contents">
+                            <p>${profile["osStyle"]}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="v-card__title">노트북 사용 선호</div>
-            <div class="v-card__text">
-                <div class="text-left">
-                    <div class="tui-editor-contents">
-                        <p>${profile["computerPrefer"]}</p>
+                <div class="v-card__title">노트북 사용 선호</div>
+                <div class="v-card__text">
+                    <div class="text-left">
+                        <div class="tui-editor-contents">
+                            <p>${profile["computerPrefer"]}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="v-card__title">휴식시간</div>
-            <div class="v-card__text">
-                <div class="text-left">
-                    <div class="tui-editor-contents">
-                        <p>${profile["breaktime"]}</p>
+                <div class="v-card__title">휴식시간</div>
+                <div class="v-card__text">
+                    <div class="text-left">
+                        <div class="tui-editor-contents">
+                            <p>${profile["breaktime"]}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="v-card__title">페어 교체주기</div>
-            <div class="v-card__text">
-                <div class="text-left">
-                    <div class="tui-editor-contents">
-                        <p>${profile["pairTurn"]}</p>
+                <div class="v-card__title">페어 교체주기</div>
+                <div class="v-card__text">
+                    <div class="text-left">
+                        <div class="tui-editor-contents">
+                            <p>${profile["pairTurn"]}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="v-card__title">일과 시간 이후</div>
-            <div class="v-card__text">
-                <div class="text-left">
-                    <div class="tui-editor-contents">
-                        <p>${profile["afterStudy"]}</p>
+                <div class="v-card__title">일과 시간 이후</div>
+                <div class="v-card__text">
+                    <div class="text-left">
+                        <div class="tui-editor-contents">
+                            <p>${profile["afterStudy"]}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="v-card__title">테스트 메소드 </div>
-            <div class="v-card__text">
-                <div class="text-left">
-                    <div class="tui-editor-contents">
-                        <p>${profile["testName"]}</p>
+                <div class="v-card__title">테스트 메소드 </div>
+                <div class="v-card__text">
+                    <div class="text-left">
+                        <div class="tui-editor-contents">
+                            <p>${profile["testName"]}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+                </div>
             </div>
         </div>
+        <!---->
     </div>
-    <!---->
 </div>
 `;
 
+/* template, constant area */
 const MUTATION_OBSERVE_CONFIG = {
     attributes: true,
     childList: true,
@@ -115,25 +117,54 @@ function generateProfileData(name, devType, osStyle, computerPrefer, breaktime, 
     }
 }
 
+// @TODO Replace to fetch api call
 async function findSurveyResultByUserName(crewGithubId) {
     return await profiles[crewGithubId];
 }
 
 /* application area */
-
-const profileExtension = new ProfileExtension();
-profileExtension.init();
-
 function ProfileExtension() {
     const $observeTarget = document.querySelector("#app.v-application");
 
-    const init = () => {
-        initMutationObserver();
-    };
+    const onClickCrewHandler = $parent => {
+        const crewName = $parent.querySelector("div.v-list-item__subtitle.caption.text-left").innerText;
+        findSurveyResultByUserName(crewName).then(profile => {
+            if (typeof profile === "undefined") {
+                alert("프로필이 존재하지 않습니다.");
+                throw Error("Profile is not exist");
+            }
 
-    function initMutationObserver() {
-        const observer = new MutationObserver(onMutationEventHandler);
-        observer.observe($observeTarget, MUTATION_OBSERVE_CONFIG);
+            let $cardBody = document.querySelector("div.v-dialog__content.v-dialog__content--active");
+            let $target = document.querySelector("div.v-dialog.v-dialog--active");
+            
+            $target.style.display = "none";
+            $cardBody.insertAdjacentHTML("beforeend", profileTemplate(profile));
+
+            $cardBody.querySelector("#back-button").addEventListener("click", () => {
+                $target.style.display = "";
+                $cardBody.removeChild($cardBody.lastElementChild);
+            });
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+    }
+
+    const crewsHandler = event => {
+        const $parent = event.target.parentNode;
+        if ($parent.className === "v-list-item__content") {
+            onClickCrewHandler($parent);
+        }
+    }
+
+    function addEventListenerToCrews(mutation) {
+        if (mutation.removedNodes.length === 1 && "v-overlay theme--dark" === mutation.removedNodes[0].className) {
+            return;
+        }
+        if (mutation.addedNodes.length === 1 && "v-dialog__content v-dialog__content--active dialog-transition-enter dialog-transition-enter-active" === mutation.addedNodes[0].className) {
+            const $userCard = mutation.target.querySelector("div.v-card__text.text-center");
+            $userCard.addEventListener("click", crewsHandler);
+        }
     }
 
     function onMutationEventHandler(mutations) {
@@ -144,49 +175,32 @@ function ProfileExtension() {
         }
     }
 
-    function addEventListenerToCrews(mutation) {
-        if (mutation.removedNodes.length === 1 && "v-overlay theme--dark" === mutation.removedNodes[0].className) {
-            console.log("removeNodes event called");
-            console.log(mutation.removedNodes[0]);
-            return;
-        }
-        if (mutation.addedNodes.length === 1 && "v-dialog__content v-dialog__content--active dialog-transition-enter dialog-transition-enter-active" === mutation.addedNodes[0].className) {
-            const $userCard = mutation.target.querySelector("div.v-card__text.text-center");
-            $userCard.addEventListener("click", crewsHandler);
-        }
+    function initMutationObserver() {
+        const observer = new MutationObserver(onMutationEventHandler);
+        observer.observe($observeTarget, MUTATION_OBSERVE_CONFIG);
     }
 
-    const crewsHandler = event => {
-        const $parent = event.target.parentNode;
-        if ($parent.className === "v-list-item__content") {
-            onClickCrewHandler($parent);
-        }
-    }
-
-    const onClickCrewHandler = $parent => {
-        const crewName = $parent.querySelector("div.v-list-item__subtitle.caption.text-left").innerText;
-        console.log(crewName);
-
-        findSurveyResultByUserName(crewName).then(profile => {
-            if (typeof profile === "undefined") {
-                alert("프로필이 존재하지 않습니다.");
-                throw Error("Profile is not exist");
+    const init = () => {
+        initMutationObserver();
+        $observeTarget.addEventListener("click", (event) => {
+            event.preventDefault();
+            if (event.target.className !== "v-overlay__scrim") {
+                return;
             }
-
-            let $target = document.querySelector("div.v-dialog.v-dialog--active");
-            const backup = $target.innerHTML;
-            $target.innerHTML = profileTemplate(profile);
-
-            $target.querySelector("button.absolute.top-30.v-btn.v-btn--flat.v-btn--text.theme--light.v-size--default").addEventListener("click", () => {
-                console.log(backup);
-                $target.innerHTML = backup;
-            });
-        }).catch((e) => {
-            console.log(e);
+            if (event.target.childList !== undefined) {
+                return;
+            }
+            let $cardBody = document.querySelector("div.v-dialog__content.v-dialog__content--active");
+            if ($cardBody.childElementCount === 2) {
+                $cardBody.removeChild($cardBody.lastElementChild);
+            }
         });
-    }
+    };
 
     return {
         init
     };
 }
+
+const profileExtension = new ProfileExtension();
+profileExtension.init();
