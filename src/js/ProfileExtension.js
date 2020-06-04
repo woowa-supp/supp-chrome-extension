fdaeb2990581603168d962edb8917477e68d1b4e
toneyparky <qkrtmddhks95@gmail.com>
2020-06-04 22:05:02 +0900

    feat: 테스트 메소드 프론트에 html로 내용 템플릿 추가

--- src/js/ProfileExtension.js
+++ src/js/ProfileExtension.js
@@ -4,19 +4,19 @@
 // import { profileTemplate } from "../utils/templates.js";
 
 const profileImageMap = {
-    "apple": "src/images/os/apple.png",
-    "linux": "src/images/os/linux.png",
-    "windows": "src/images/os/windows.png",
-    "code_guardian": "src/images/type/CODE_GUARDIAN.png",
-    "macgyver": "src/images/MACGYVER.png",
-    "mad_scientist": "src/images/type/MAD_SCIENTIST.png",
-    "ninja": "src/images/type/NINJA.png",
-    "the_architect": "src/images/type/THE_ARCHITECT.png"
+  "apple": "src/images/os/apple.png",
+  "linux": "src/images/os/linux.png",
+  "windows": "src/images/os/windows.png",
+  "code_guardian": "src/images/type/CODE_GUARDIAN.png",
+  "macgyver": "src/images/MACGYVER.png",
+  "mad_scientist": "src/images/type/MAD_SCIENTIST.png",
+  "ninja": "src/images/type/NINJA.png",
+  "the_architect": "src/images/type/THE_ARCHITECT.png"
 };
 
-const profileTemplate = profile => 
-`
-<div class="v-dialog v-dialog--active" style="width: 700px;">
+const profileTemplate = profile =>
+  `
+<div class="v-dialog v-dialog--active" style="width: 600px;">
     <div class="v-card v-sheet theme--light">
         <div class="v-card__title headline">
             <button type="button" id="back-button" class="absolute top-30 v-btn v-btn--flat v-btn--text theme--light v-size--default">
@@ -41,17 +41,12 @@ const profileTemplate = profile =>
             <div>
                 <img src="https://avatars1.githubusercontent.com/u/20358042?s=400&u=cf1c0ad6fabec31beea9f2d5a64700e0d2f81203&v=4" alt="">            
             </div>
+            
+            <div class="v-card__title">${profile["messageToCrew"]}</div>
+            <hr>
+            
             <div class="row">
                 <div class="col col-12">
-
-                <div class="v-card__title">운영체제</div>
-                <div class="v-card__text">
-                    <div class="text-left">
-                        <div class="tui-editor-contents">
-                            <p>${profile["osStyle"]}</p>
-                        </div>
-                    </div>
-                </div>
 
                 <div class="v-card__title">노트북 사용 선호</div>
                 <div class="v-card__text">
@@ -93,128 +88,153 @@ const profileTemplate = profile =>
                 <div class="v-card__text">
                     <div class="text-left">
                         <div class="tui-editor-contents">
-                            <p>${profile["testName"]}</p>
+<!--                            <p>${profile["testName"]}</p>-->
+         <div class="input-answer" style="background-color: rgb(63, 68, 71); padding: 2rem;">
+         <p>
+         <span style="font-size: 1.25rem; color: orange" >
+            @DisplayName(
+            <span style="font-size: 1.25rem; color: #fffafa">
+                "DisplayName을 사용하지 않는다면 작성하지 않아도 됩니다."
+            </span>
+            )<br>
+         </span> 
+         <span style="font-size: 1.25rem; color: orange">@Test</span>
+         </p>
+         <p>
+         <span style="font-size: 1.25rem; color: snow">void <span class="method-name-container">${profile["testName"]}</span> <span style="color: #fffafa">() {</span> </span><br>
+         <span style="font-size: 1.25rem; color: darkgrey">…</span><br>
+         <span style="font-size: 1.25rem; color: snow">}</span>
+         </p>
+        </div>                        
+            </div>
+                </div>
+                <div class="v-card__title">깃헙 커밋 메시지 </div>
+                <div class="v-card__text">
+                    <div class="text-left">
+                        <div class="tui-editor-contents">
+                            <p>${profile["gitConvention"]}</p>
                         </div>
                     </div>
                 </div>
-
                 </div>
             </div>
         </div>
-        <!---->
     </div>
 </div>
 `;
 
 /* template, constant area */
 const MUTATION_OBSERVE_CONFIG = {
-    attributes: true,
-    childList: true,
-    characterData: true
+  attributes: true,
+  childList: true,
+  characterData: true
 };
 
 const profiles = {
-    "ks-kim": generateProfileData("ks-kim", "ninja", "apple", "아무거나", "빈백조아", "기능단위로 진행", "6시 이후에는 페어 진행하지 않기", "테스트 명명법"),
-    "toneyparky": generateProfileData("toneyparky", "the_architect", "windows", "아무거나", "빈백매우조아", "5분 간격으로 진행", "6시 이후에도 쌉가능", "테스트 명명법"),
-    "lalize": generateProfileData("lalize", "the_architect", "linux", "아무거나", "빈백매우조아", "5분 간격으로 진행", "6시 이후에도 쌉가능", "테스트 명명법")
+  "ks-kim": generateProfileData("ks-kim", "ninja", "apple", "아무거나", "빈백조아", "기능단위로 진행", "6시 이후에는 페어 진행하지 않기", "들어갈 값들이 있다.", "들어갈 값들이 있다.", "나는 멋쟁이"),
+  "toneyparky": generateProfileData("toneyparky", "the_architect", "windows", "아무거나", "빈백매우조아", "5분 간격으로 진행", "6시 이후에도 쌉가능", "들어갈 값들이 있다.", "들어갈 값들이 있다.", "코딩이 행복한 남자 코행남"),
+  "lalize": generateProfileData("lalize", "the_architect", "linux", "아무거나", "빈백매우조아", "5분 간격으로 진행", "6시 이후에도 쌉가능", "들어갈 값들이 있다.", "feature: ", "헬스보이")
 }
 
-function generateProfileData(name, developerType, osStyle, computerPrefer, breaktime, pairTurn, afterStudy, testName) {
-    return {
-        "login": name,
-        "developerType": developerType,
-        "osStyle": osStyle,
-        "computerPrefer": computerPrefer,
-        "breaktime": breaktime,
-        "pairTurn": pairTurn,
-        "afterStudy": afterStudy,
-        "testName": testName
-    }
+function generateProfileData(name, developerType, osStyle, computerPrefer, breaktime, pairTurn, afterStudy, testName, gitConvention, messageToCrew) {
+  return {
+    "login": name,
+    "developerType": developerType,
+    "osStyle": osStyle,
+    "computerPrefer": computerPrefer,
+    "breaktime": breaktime,
+    "pairTurn": pairTurn,
+    "afterStudy": afterStudy,
+    "testName": testName,
+    "gitConvention": gitConvention,
+    "messageToCrew": messageToCrew
+  }
 }
 
 // @TODO Replace to fetch api call
 async function findSurveyResultByUserName(crewGithubId) {
-    return await profiles[crewGithubId];
+  return await profiles[crewGithubId];
 }
 
 /* application area */
 function ProfileExtension() {
-    const $observeTarget = document.querySelector("#app.v-application");
+  const $observeTarget = document.querySelector("#app.v-application");
 
-    const onClickCrewHandler = $parent => {
-        const crewName = $parent.querySelector("div.v-list-item__subtitle.caption.text-left").innerText;
-        findSurveyResultByUserName(crewName).then(profile => {
-            if (typeof profile === "undefined") {
-                alert("프로필이 존재하지 않습니다.");
-                throw Error("Profile is not exist");
-            }
+  const onClickCrewHandler = $parent => {
+    const crewName = $parent.querySelector("div.v-list-item__subtitle.caption.text-left").innerText;
+    findSurveyResultByUserName(crewName).then(profile => {
+      if (typeof profile === "undefined") {
+        alert("프로필이 존재하지 않습니다.");
+        throw Error("Profile is not exist");
+      }
 
-            let $cardBody = document.querySelector("div.v-dialog__content.v-dialog__content--active");
-            let $target = document.querySelector("div.v-dialog.v-dialog--active");
-            
-            $target.style.display = "none";
-            $cardBody.insertAdjacentHTML("beforeend", profileTemplate(profile));
+      let $cardBody = document.querySelector("div.v-dialog__content.v-dialog__content--active");
+      let $target = document.querySelector("div.v-dialog.v-dialog--active");
 
-            $cardBody.querySelector("#back-button").addEventListener("click", () => {
-                $target.style.display = "";
-                $cardBody.removeChild($cardBody.lastElementChild);
-            });
-        })
-        .catch((e) => {
-            console.log(e);
-        });
+      $target.style.display = "none";
+      $cardBody.insertAdjacentHTML("beforeend", profileTemplate(profile));
+
+      $cardBody.querySelector("#back-button").addEventListener("click", () => {
+        $target.style.display = "";
+        $cardBody.removeChild($cardBody.lastElementChild);
+      });
+    })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  const crewsHandler = event => {
+    const $parent = event.target.parentNode;
+    if ($parent.className === "v-list-item__content") {
+      onClickCrewHandler($parent);
     }
+  }
 
-    const crewsHandler = event => {
-        const $parent = event.target.parentNode;
-        if ($parent.className === "v-list-item__content") {
-            onClickCrewHandler($parent);
-        }
+  function addEventListenerToCrews(mutation) {
+    if (mutation.removedNodes.length === 1 && "v-overlay theme--dark" === mutation.removedNodes[0].className) {
+      return;
     }
-
-    function addEventListenerToCrews(mutation) {
-        if (mutation.removedNodes.length === 1 && "v-overlay theme--dark" === mutation.removedNodes[0].className) {
-            return;
-        }
-        if (mutation.addedNodes.length === 1 && "v-dialog__content v-dialog__content--active dialog-transition-enter dialog-transition-enter-active" === mutation.addedNodes[0].className) {
-            const $userCard = mutation.target.querySelector("div.v-card__text.text-center");
-            $userCard.addEventListener("click", crewsHandler);
-        }
+    if (mutation.addedNodes.length === 1 && "v-dialog__content v-dialog__content--active dialog-transition-enter dialog-transition-enter-active" === mutation.addedNodes[0].className) {
+      const $userCard = mutation.target.querySelector("div.v-card__text.text-center");
+      $userCard.addEventListener("click", crewsHandler);
     }
+  }
 
-    function onMutationEventHandler(mutations) {
-        try {
-            addEventListenerToCrews(mutations[0]);
-        } catch(e) {
-            console.log(e);
-        }
+  function onMutationEventHandler(mutations) {
+    try {
+      addEventListenerToCrews(mutations[0]);
+      // todo 추가 기능
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    function initMutationObserver() {
-        const observer = new MutationObserver(onMutationEventHandler);
-        observer.observe($observeTarget, MUTATION_OBSERVE_CONFIG);
-    }
+  function initMutationObserver() {
+    const observer = new MutationObserver(onMutationEventHandler);
+    observer.observe($observeTarget, MUTATION_OBSERVE_CONFIG);
+  }
 
-    const init = () => {
-        initMutationObserver();
-        $observeTarget.addEventListener("click", (event) => {
-            event.preventDefault();
-            if (event.target.className !== "v-overlay__scrim") {
-                return;
-            }
-            if (event.target.childList !== undefined) {
-                return;
-            }
-            let $cardBody = document.querySelector("div.v-dialog__content.v-dialog__content--active");
-            if ($cardBody.childElementCount === 2) {
-                $cardBody.removeChild($cardBody.lastElementChild);
-            }
-        });
-    };
+  const init = () => {
+    initMutationObserver();
+    $observeTarget.addEventListener("click", (event) => {
+      event.preventDefault();
+      if (event.target.className !== "v-overlay__scrim") {
+        return;
+      }
+      if (event.target.childList !== undefined) {
+        return;
+      }
+      let $cardBody = document.querySelector("div.v-dialog__content.v-dialog__content--active");
+      if ($cardBody.childElementCount === 2) {
+        $cardBody.removeChild($cardBody.lastElementChild);
+      }
+    });
+  };
 
-    return {
-        init
-    };
+  return {
+    init
+  };
 }
 
 const profileExtension = new ProfileExtension();
