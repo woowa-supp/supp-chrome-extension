08039bb4361882eaafb19cb1332e77ce0d8b913e
K.S.KIM <nullable@kakao.com>
2020-06-08 20:38:12 +0900

    refactor: 이미지 리소스 패키지 변경

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,12 +9,12 @@ export const EVENT_TYPE = {
 };
 
 export const PROFILE_IMAGE_MAP = {
-    "apple": "src/images/os/apple.png",
-    "linux": "src/images/os/linux.png",
-    "windows": "src/images/os/windows.png",
-    "code_guardian": "src/images/type/CODE_GUARDIAN.png",
-    "macgyver": "src/images/MACGYVER.png",
-    "mad_scientist": "src/images/type/MAD_SCIENTIST.png",
-    "ninja": "src/images/type/NINJA.png",
-    "the_architect": "src/images/type/THE_ARCHITECT.png"
+    "apple": "src/assets/images/os/apple.png",
+    "linux": "src/assets/images/os/linux.png",
+    "windows": "src/assets/images/os/windows.png",
+    "code_guardian": "src/assets/images/type/CODE_GUARDIAN.png",
+    "macgyver": "src/assets/images/MACGYVER.png",
+    "mad_scientist": "src/assets/images/type/MAD_SCIENTIST.png",
+    "ninja": "src/assets/images/type/NINJA.png",
+    "the_architect": "src/assets/images/type/THE_ARCHITECT.png"
 };
