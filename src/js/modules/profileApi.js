f3d64baa8b5daa7ef68117513eb576b80d239718
K.S.KIM <nullable@kakao.com>
2020-06-16 09:56:55 +0900

    chore: 잡다한 요소 개선
    - 불필요한 파일 제거
    - 불필요한 콘솔 출력문 제거

--- src/js/modules/profileApi.js
+++ src/js/modules/profileApi.js
@@ -4,7 +4,6 @@ const DEV_TYPE_IMAGE_URL = 'http://52.78.188.23:8080/';
 const api = (() => {
     const request = (url) =>
         fetch(url).then((response) => {
-            console.log(url, response);
             if (!response.ok) {
                 throw Error('프로필을 찾지 못했습니다.');
             }
