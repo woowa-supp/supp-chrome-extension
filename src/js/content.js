ea7d565f2912021dda2fdb46f05717fa4a6b0019
K.S.KIM <nullable@kakao.com>
2020-09-07 23:54:43 +0900

    fix: v-application이 없는 페이지 임시 예외처리

--- src/js/content.js
+++ src/js/content.js
@@ -1,4 +1,9 @@
 (async () => {
-  const src = chrome.runtime.getURL('src/js/ProfileApplication.js');
-  await import(src);
+  try {
+    const src = chrome.runtime.getURL('src/js/ProfileApplication.js');
+    await import(src);
+  } catch(e) {
+    // application이 없는 페이지 예외가 발생하지 않도록 처리
+    // URL matching pattern 변경 필요
+  }
 })();
